2fd00ec5ab153f213bf3606aec7ebc01a00c5d44
Mattias Ljung <mljung02@gmail.com>
2015-07-26 21:01:02 -0600

    scoring

--- public/javascripts/minimax.js
+++ public/javascripts/minimax.js
@@ -12,6 +12,33 @@ var score = function (turn) {
   }
 }
 
+var checkDiagonal = function () {
+  if (board[1][1] > 0) {
+    if (board[0][0] === board[1][1] && board[2][2] === board[1][1]){
+      return true;
+    }
+    if (board[0][2] === board[1][1] && board[2][0] === board[1][1]) {
+      return true;
+    }
+  }
+}
+
+var checkVertical = function () {
+  for (var i = 0; i < board[0].length; i++) {
+    if (board[0][i] === board[1][i] && board[2][i] === board[0][i] && board[0][i] > 0 ) {
+      return true;
+    }
+  }
+}
+
+var checkHorizontal = function () {
+  for (var i = 0; i < board.length; i++) {
+    if (board[i][0] === board[i][1] && board[i][2] === board[i][0] && board[i][0] > 0 ) {
+      return true;
+    }
+  }
+}
+
 //create tree structure for moves, and then traverse recursively via minimax.
 
 var board = ["00","01","02","10","11","12","20","21","22"]
@@ -42,7 +69,7 @@ var build = function(moves, obj, depth, move) {
   return obj
 }
 
-build({}, smallboard)
+var moveset = build({}, smallboard)
 
 // var minimax = function (board) {
 //   var boardTemp = board
